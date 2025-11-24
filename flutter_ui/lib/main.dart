752d11bb9738e1394b38d68c3d71aecac0015aaf
eylulcankesici <github-eylulcankesici@gmail.com>
2025-11-24 16:58:43 +0300

    aferin

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -21,7 +21,7 @@ class AetherApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.dark,  // ⚫ Siyah tema aktif
       home: const HomePage(),
     );
   }
